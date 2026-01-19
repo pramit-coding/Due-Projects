3ddf91f895ddb905ed6fa22ecf8160516adda7ac
pramit-coding <manojpanchal7@gmail.com>
2026-01-19 16:47:12 +1000

    Changes

--- Expression.py
+++ Expression.py
@@ -1,6 +1,15 @@
+class ADD:
+    def __init__(self,num1,num2,num3):
+        self.num1=num1
+        self.num2=num2
+        self.num3=num3
+    def total(self):
+        print("The sum is",self.num1 + self.num2 + self.num3)
 
-num1 = 10
-num2 = 20
-num3 = 40
+calc = ADD(10,20,40)
+calc.total()
 
-print("The sum is", num1 + num2 + num3)
+
+
+
+
